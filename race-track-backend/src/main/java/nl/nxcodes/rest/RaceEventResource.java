8eed4b81ffcaadb372a409ba4097fe6df56e8dc3
ncks03 <n.schuit@icloud.com>
2026-03-31 23:23:52 +0200

    Implemented api endpoint for getting a single event

--- race-track-backend/src/main/java/nl/nxcodes/rest/RaceEventResource.java
+++ race-track-backend/src/main/java/nl/nxcodes/rest/RaceEventResource.java
@@ -7,12 +7,16 @@ import jakarta.ws.rs.core.Response;
 import nl.nxcodes.model.domain.RaceEvent;
 import nl.nxcodes.model.repository.RaceEventRepository;
 import nl.nxcodes.rest.dto.post.RaceEventPostDTO;
+import nl.nxcodes.rest.dto.response.RaceEventResponseDTO;
 
 import java.net.URI;
 import java.util.List;
+import java.util.UUID;
 
 @Path("/events")
 public class RaceEventResource {
+
+    private static final String EVENTS_ENDPOINT = "/api/events";
 
     private final RaceEventRepository raceEventRepository;
 
@@ -24,7 +28,10 @@ public class RaceEventResource {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response getRaceEvents() {
-        List<RaceEvent> raceEvents = raceEventRepository.findAll();
+
+        List<RaceEventResponseDTO> raceEvents = raceEventRepository.findAll().stream()
+                .map(RaceEventResponseDTO::fromEntity)
+                .toList();
 
         // Throw exception on not found for mapper to pick up
         if (raceEvents.isEmpty()) {
@@ -34,11 +41,24 @@ public class RaceEventResource {
         return Response.ok(raceEvents).build();
     }
 
+    @GET
+    @Path("/{uuid}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getRaceEvent(@PathParam("uuid") UUID uuid) {
+
+        RaceEventResponseDTO event = raceEventRepository.findByUuid(uuid)
+                .map(RaceEventResponseDTO::fromEntity)
+                .orElseThrow(() -> new WebApplicationException("No race event found for id", Response.Status.NOT_FOUND));
+
+        return Response.ok(event).build();
+    }
+
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     public Response createRaceEvent(RaceEventPostDTO raceEventDto) {
+
         RaceEvent saved = raceEventRepository.save(new RaceEvent(raceEventDto));
 
-        return Response.created(URI.create("/api/events/" + saved.getUuid())).build();
+        return Response.created(URI.create(EVENTS_ENDPOINT + saved.getUuid())).build();
     }
 }
